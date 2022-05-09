e41a25345a902d9483073a31851d4292ddd9e3f9
Nikita <kerzeenok.nikita@gmail.com>
2022-05-09 19:42:37 +0300

    delete useless сom

--- publication_app/forms/add_post.py
+++ publication_app/forms/add_post.py
@@ -1,5 +1,4 @@
 from django import forms
-from publication_app.models import Post
 
 
 class AddPostForm(forms.Form):
@@ -7,13 +6,3 @@ class AddPostForm(forms.Form):
     text = forms.CharField(label='Текст к посту')
     imagine = forms.ImageField(label='Выберите фото',  required=False)
     is_public = forms.BooleanField(label='Публичная запись', initial=True)
-
-# class AddPostForm(forms.ModelForm):
-#     # title = forms.CharField(label='Заголовок поста', max_length=256)
-#     # text = forms.CharField(label='Текст к посту')
-#     # imagine = forms.ImageField(label='Выберите фото',  required=False)
-#     # is_public = forms.BooleanField(label='Публичная запись', initial=True)
-#
-#     class Meta:
-#         model = Post
-#         fields = ['title', 'text', 'imagine', 'is_public']
