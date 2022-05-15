2c20e5b38abe6d5b9eba0db4a7270cd391d292d3
Nikita <kerzeenok.nikita@gmail.com>
2022-05-15 15:17:02 +0300

    Edit post with several image

--- publication_app/urls.py
+++ publication_app/urls.py
@@ -3,28 +3,29 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
-from publication_app.views.account import account
-from publication_app.views.login import user_login
+from publication_app.views.account import Account
+from publication_app.views.login import UserLogin
 from publication_app.views.main_page import main_page
-from publication_app.views.register import register
+from publication_app.views.register import Register
 from publication_app.views.user_logout import user_logout
-from publication_app.views.user_redaction import user_redaction
-from publication_app.views.your_account import your_account
-from publication_app.views.add_post import add_post
-from publication_app.views.edit_post import EditPost
+from publication_app.views.user_redaction import UserRedaction
+from publication_app.views.your_account import YourAccount
+from publication_app.views.add_post import AddPost
+from publication_app.views.edit_post import EditImagePost
 from publication_app.views.delete_post import delete_post
 
 urlpatterns = [
     path('', main_page, name='main_page'),
-    path('account/', account, name='account'),
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
+    path('account/', Account.as_view(), name='account'),
+    path('register/', Register.as_view(), name='register'),
+    path('login/', UserLogin.as_view(), name='login'),
     path('logout/', user_logout, name='logout'),
-    path('redaction/', user_redaction, name='redaction'),
-    path('your_account/', your_account, name='your_account'),
-    path('add_post/', add_post, name='add_post'),
-    path('edit_post/<int:pk>', EditPost.as_view(), name ='edit_post'),
+    path('redaction/', login_required(UserRedaction.as_view()), name='redaction'),
+    path('your_account/', login_required(YourAccount.as_view()), name='your_account'),
+    path('add_post/', login_required(AddPost.as_view()), name='add_post'),
+    path('edit_post/<int:pk>', login_required(EditImagePost.as_view()), name='edit_post'),
     path('delete_post/<int:pk>', delete_post, name='delete_post')
 ]
 
--- publication_app/views/edit_post.py
+++ publication_app/views/edit_post.py
@@ -1,9 +1,43 @@
-from django.views.generic import UpdateView
-from publication_app.models import Post
+from django.views import View
+from django.forms import modelformset_factory
+from publication_app.forms.EditPostForm import EditPostForm
+from django.shortcuts import render, redirect
+from publication_app.models import Post, ImagePost
 
 
-class EditPost(UpdateView):
-    model = Post
-    template_name = 'edit_post.html'
-    fields = ['title', 'text', 'is_public', 'imagine']
+class EditImagePost(View):
+    ImageFormSet = modelformset_factory(ImagePost, fields={"image", })
 
+    def get(self, request, pk):
+        get_post = Post.objects.get(pk=pk)
+
+        post_form = EditPostForm(instance=get_post)
+        image_form = self.ImageFormSet(queryset=ImagePost.objects.filter(post=get_post))
+        context = {
+            "title": "Добавить пост",
+            "form": post_form,
+            "image": image_form
+        }
+        return render(request, "edit_post.html", context)
+
+    def post(self, request, pk):
+        get_post = Post.objects.get(pk=pk)
+
+        post_form = EditPostForm(data=request.POST, instance=get_post)
+
+        form_image = self.ImageFormSet(request.POST or None, request.FILES or None)
+        get_image = ImagePost.objects.filter(post=get_post)
+        if post_form.is_valid() and form_image.is_valid():
+            post_form.save()
+            for i, file in enumerate(form_image):
+                if file.cleaned_data:
+                    if file.cleaned_data["id"] is None:
+                        ImagePost(post=get_post, image=file.cleaned_data.get('image')).save()
+                    elif file.cleaned_data["image"] is False:
+                        ImagePost.objects.get(id=request.POST.get(f'form-{i}-id')).delete()
+                    else:
+                        image = ImagePost(post=get_post, image=file.cleaned_data.get('image'))
+                        obj_img = ImagePost.objects.get(id=get_image[i].id)
+                        obj_img.image = image.image
+                        obj_img.save()
+            return redirect('account')
